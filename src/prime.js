8940a4f043d6de3d92b68a3a18e5fcd4597cc784
masha yanson <mashayanson@gmail.com>
2023-04-07 16:46:13 +0200

    добавлено правило для 1 в простых числах

--- src/prime.js
+++ src/prime.js
@@ -4,7 +4,7 @@ import { getRandomInt } from './question2.js';
 
 const isPrime = (number) => {
   for (let i = 2; i < number; i += 1) {
-    if (number % i === 0) {
+    if (number % i === 0 || number === 1) {
       return false;
     }
   }
