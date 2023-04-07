27134307f212ecd8a1adab13d7648ce301e3fbf8
masha yanson <mashayanson@gmail.com>
2023-04-07 15:53:03 +0200

    запятая

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 
 const usernameQuestion = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
   return userName;
 };
 
