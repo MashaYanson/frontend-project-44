7827380307e993c5086acdbf384cdc4bda2664da
masha yanson <mashayanson@gmail.com>
2023-04-19 15:07:21 +0200

    исправлена ошибка в gcd

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import runGame from '../src/utils/runGame';
+import runGame from '../src/utils/runGame.js';
 import gcd from '../src/games/gcd.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
